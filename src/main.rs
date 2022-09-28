040cfbd585b55713868dde8cfa35d5070bb7639b
Cory Nivens <cgn7634@gmail.com>
2022-09-27 23:24:48 -0400

    Fixed FromStr implementation on query type

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
+use std::str::FromStr;
+
+pub fn main() {
+    println!("hello world");
+}
+
 enum QueryType {
-    CREATE
-    SELECT
-    INSERT
-    UPDATE
-    DELETE
+    CREATE,
+    SELECT,
+    INSERT,
+    UPDATE,
+    DELETE,
 }
 
 impl FromStr for QueryType {
     type Err = String;
 
-    fn from_str(input: &str) -> Result<QueryType, Self.Err> {
-        match input {
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        match input.to_uppercase().as_str() { // not case-sensitive, so force case
             "CREATE" => Ok(QueryType::CREATE),
             "SELECT" => Ok(QueryType::SELECT),
             "INSERT" => Ok(QueryType::INSERT),
